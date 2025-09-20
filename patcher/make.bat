bdad996fedf69696c62e2658258e2dc9fb8b4cbe
friedrichOsDev <f39597054@gmail.com>
2025-09-20 13:34:36 +0200

    I made the make.bat generall and not only for 'IEUser'

--- patcher/make.bat
+++ patcher/make.bat
@@ -1,14 +1,16 @@
+@echo off
+
+set current_path=%~dp0
 
 C:
 cd C:\MinGW\bin
-mkdir C:\Users\IEUser\Desktop\Snail2021\
-rem gcc -o F:\Snail202x\Snail2021.exe F:\Snail202x\Snail2021.c -I \MinGW\msys\1.0\include -L \MinGW\msys\1.0\lib -lz -static
-gcc -o C:\Users\IEUser\Desktop\Snail2021\Snail2021.exe F:\hollyhock-2\patcher\Snail2021mod.c -lz -static
-del C:\Users\IEUser\Desktop\Snail2021\mod.txt
+
+mkdir "%USERPROFILE%\Desktop\Snail2021"
+
+gcc -o "%USERPROFILE%\Desktop\Snail2021\Snail2021.exe" "%current_path%Snail2021mod.c" -lz -static
 
 pause
 
-copy C:\Users\IEUser\Desktop\Snail2021\Snail2021.exe F:\hollyhock-2\patcher\Snail2021.exe
+copy "%USERPROFILE%\Desktop\Snail2021\Snail2021.exe" "%current_path%Snail2021.exe"
 
 pause
-
