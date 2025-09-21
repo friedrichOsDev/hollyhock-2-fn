571b22009734aa266b8f10cf326e62f4d9f96f3d
friedrichOsDev <f39597054@gmail.com>
2025-09-21 12:54:45 +0200

    Fixed missing files and updated auto build file

--- auto_build/build.bat
+++ auto_build/build.bat
@@ -1,6 +1,11 @@
 @echo off
+set cpath=%~dp0
+set hollyhock-dir=%cpath%..\
+set output-dir=%USERPROFILE%\Desktop\Snail2021
 echo "Building Snail2021.exe..."
 cd ../patcher
 ./make.bat
 cd ../auto_build
+copy %hollyhock-dir%\auto_build\run.bin %output-dir%\run.bin
+copy %hollyhock-dir%\auto_build\python.bin %output-dir%\python.bin
 echo "Build complete."
