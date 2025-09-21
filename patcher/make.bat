0de18330c1427d606511dae182422e06d2e2a213
friedrichOsDev <f39597054@gmail.com>
2025-09-21 10:22:06 +0200

    test of my first idea of successfully disable exam-mode block

---
+++ patcher/make.bat
@@ -0,0 +1,16 @@
+@echo off
+set hollyhock-dir=C:\Users\Friedrich\Desktop\hollyhock-2
+set output-dir=C:\Users\Friedrich\Desktop\Snail2021
+
+C:
+cd C:\MinGW\bin
+mkdir %output-dir%
+gcc -o %output-dir%\Snail2021.exe %hollyhock-dir%\patcher\Snail2021mod.c -lz -static
+del %output-dir%\mod.txt
+
+pause
+
+copy %output-dir%\Snail2021.exe %hollyhock-dir%\patcher\Snail2021.exe
+
+pause
+
